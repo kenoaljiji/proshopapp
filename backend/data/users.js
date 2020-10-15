e4b43fac67f41c52fd050f034e0bba42beb2e445
kenoaljiji <67831469+kenoaljiji@users.noreply.github.com>
2020-10-15 16:26:01 +0200

    Models in datebase

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const user = [
+    {
+        name:'Admin User',
+        email: 'admin@example.com',
+        password: xxxx,
+        isAdmin: true
+    },
+    {
+        name:'John Doe',
+        email: 'john@example.com',
+        password: xxxx,
+    },
+    {
+        name:'Jane Doe',
+        email: 'john@example.com',
+        password: xxxx,
+    },
+]
