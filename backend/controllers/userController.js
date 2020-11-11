727fd01839cdf7055b049bfd422747b7154f2c5c
kenoaljiji <67831469+kenoaljiji@users.noreply.github.com>
2020-11-11 23:36:52 +0100

    Login and Registration Form setup with backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,7 +33,7 @@ const authUser = asyncHandler(async (req, res) => {
 //@access Public
 
 const registerUser = asyncHandler(async (req, res) => {
-   const { name,email, password } = req.body
+   const { name ,email, password } = req.body
 
    const userExist = await User.findOne({ email: email })
 
@@ -87,7 +87,42 @@ const getUserProfile = asyncHandler(async (req, res) => {
    
  })
 
+// @desc Update user profile
+// PUT /api/users/profile
+//@access Private
+
+const UpdateUserProfile = asyncHandler(async (req, res) => {
+    
+    //This will getz user
+    const user = await User.findById(req.user._id)
+
+
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if(req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name:updatedUser.name,
+            email:updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        })
+
+
+    } else {
+        res.status(404)
+        throw new Error('User is not found')
+    }
+   
+ })
+
 
 export {
-    authUser, registerUser, getUserProfile
+    authUser, registerUser, getUserProfile, UpdateUserProfile
 }
