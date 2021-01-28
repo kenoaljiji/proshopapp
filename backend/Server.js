c57c1d5e5331137e1e16b85fcf41c1d180a1dc0f
kenoaljiji <67831469+kenoaljiji@users.noreply.github.com>
2021-01-28 19:45:34 +0100

    fix heroku-postbuild script

--- backend/Server.js
+++ backend/Server.js
@@ -34,20 +34,21 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
-    app.get('/', (req, res) => {
-        res.send('API is running...')
-    })
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
-app.use(notFound);
+app.use(notFound)
+app.use(errorHandler)
 
-app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 5000
